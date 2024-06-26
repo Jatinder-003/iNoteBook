e20f91d74651fdfa6ff48deb40262b17465fd47a
Jatinder-003 <jatinderkumarjmmu@gmail.com>
2024-06-26 20:21:01 +0530

    Second commit

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,13 +1,15 @@
 const connectToMongo = require("./db");
-const express = require('express')
+const express = require("express");
 connectToMongo();
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello JK!')
-})
+app.use(express.json())
+
+//Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
